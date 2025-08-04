79526f1d5da5780e37919d329a71ac13ad9517b8
Nikita Grigorev <pettyaelse@gmail.com>
2025-08-04 16:32:50 +0700

    w

--- Assets/Components/UIOverlay.cs
+++ Assets/Components/UIOverlay.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Text;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using ColorUtility = UnityEngine.ColorUtility;
 
