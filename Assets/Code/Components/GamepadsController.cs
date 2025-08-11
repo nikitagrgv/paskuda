4d6bdc11e3ef19265908e1b5450715a4a2b3efec
Nikita Grigorev <pettyaelse@gmail.com>
2025-08-11 20:05:26 +0700

    w

---
+++ Assets/Code/Components/GamepadsController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class GamepadsController : MonoBehaviour
+{
+    void Start()
+    {
+        List<Gamepad> xBoxGamepads = new();
+        List<Gamepad> otherGamepads = new();
+
+        ReadOnlyArray<Gamepad> gamepads = Gamepad.all;
+        foreach (Gamepad gamepad in gamepads)
+        {
+            Debug.Log($"Detected gamepad: {gamepad.name} (Path: {gamepad.device.path})");
+            if (gamepad.name.StartsWith("XInput"))
+            {
+                xBoxGamepads.Add(gamepad);
+            }
+            else
+            {
+                otherGamepads.Add(gamepad);
+            }
+        }
+
+        if (xBoxGamepads.Count > 0)
+        {
+            foreach (Gamepad gamepad in otherGamepads)
+            {
+                Debug.Log($"Removed gamepad: {gamepad.name} (Path: {gamepad.device.path})");
+                InputSystem.RemoveDevice(gamepad.device);
+            }
+        }
+    }
+}
