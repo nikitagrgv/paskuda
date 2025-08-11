314a5b9f1a5aea418a51c2ffb812bb8e49e21c1e
Nikita Grigorev <pettyaelse@gmail.com>
2025-08-11 20:30:07 +0700

    w

--- Assets/Code/Components/Arsenal.cs
+++ Assets/Code/Components/Arsenal.cs
@@ -1,16 +1,11 @@
+using System.Collections.Generic;
+using Code.Weapons;
 using UnityEngine;
 
-public class Arsenal : MonoBehaviour
+namespace Code.Components
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Arsenal : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<Weapon> weapons;
     }
 }
---
+++ Assets/Code/Weapons/Weapon.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Code.Weapons
+{
+    [Serializable]
+    public class Weapon
+    {
+        private string Name { get; set; }
+    }
+}
