312620067f5777f0b1468b52c27e54657d30dc3c
Nikita Grigorev <pettyaelse@gmail.com>
2025-08-12 01:14:03 +0700

    w

--- Assets/Code/Components/Arsenal.cs
+++ Assets/Code/Components/Arsenal.cs
@@ -22,7 +22,7 @@ namespace Code.Components
 
         public WeaponMeta GetRandomWeapon()
         {
-            if (_calculated)
+            if (!_calculated)
             {
                 Recalculate();
             }
